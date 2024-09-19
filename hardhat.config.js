34e44c5b0b93c910364c8db11216d5b96acbf3d2
Rahul Kumar Pandey <rahulpandey1630@gmail.com>
2024-09-20 00:15:28 +0530

    Create hardhat.config.js

---
+++ hardhat.config.js
@@ -0,0 +1,24 @@
+/** @type import('hardhat/config').HardhatUserConfig */
+require("@nomiclabs/hardhat-ethers");
+module.exports = {
+  solidity: "0.8.18",
+};
+require("@nomiclabs/hardhat-waffle");
+require("dotenv").config();
+
+/** @type import('hardhat/config').HardhatUserConfig */
+module.exports = {
+  solidity: "0.8.18",
+  networks:{
+    hardhat: {},
+    sepolia: {
+      url: 'https://ethereum-sepolia-rpc.publicnode.com',
+      accounts: [process.env.PRIVATE_KEY]
+    },   
+amoy: {
+      url: 'https://ethereum-sepolia-rpc.publicnode.com',
+      accounts: [process.env.PRIVATE_KEY]
+},
+
+}
+};
